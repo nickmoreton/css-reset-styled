7a5a280bd4a1193bbd93c2836ebb079b09e29e35
nickmoreton <nick.moreton@torchbox.com>
2024-09-05 22:48:02 +0100

    Base styles and content

---
+++ js/navigation.js
@@ -0,0 +1,28 @@
+customElements.define('custom-navigation', class Navigation extends HTMLElement {
+    /**
+     * Constructor for the custom navigation element.
+     * Calls the parent constructor, HTMLElement().
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * Called when the component is inserted into the DOM tree.
+     * Populates the innerHTML of the component with the links to the
+     * different components.
+     */
+    connectedCallback() {
+        this.innerHTML = `
+              <nav class="custom">
+                  <ul>
+                      <li><a href="/">Index</a></li>
+                      <li><a href="components-text.html">Text</a></li>
+                      <li><a href="components-media.html">Media</a></li>
+                      <li><a href="components-table.html">Tables</a></li>
+                      <li><a href="components-form.html">Forms</a></li>
+                  </ul>
+              </nav>
+                  `;
+    }
+});
---
+++ js/style.js
@@ -0,0 +1,31 @@
+customElements.define('style-js', class extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `
+        <link rel="stylesheet" href="css/config.css">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/guides.css">
+        <link rel="stylesheet" href="css/table.css">
+        <link rel="stylesheet" href="css/forms.css">
+        <style>
+        nav.custom {
+            box-shadow: none;
+        }
+        nav.custom ul {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+            display: flex;
+            justify-content: center;
+            gap: 1rem;
+            box-shadow: none;
+        }
+        nav.custom ul li {
+            box-shadow: none;
+        }
+        nav.custom ul li a {
+            box-shadow: none;
+        }
+        </style>`;
+    }
+});
