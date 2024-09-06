c7fb7796f00f1f2c41552dbbedac10b6f4ab041e
nickmoreton <nick.moreton@torchbox.com>
2024-09-06 09:30:19 +0100

    A tidy up of forms

--- js/style.js
+++ js/style.js
@@ -26,6 +26,15 @@ customElements.define('style-js', class extends HTMLElement {
         nav.custom ul li a {
             box-shadow: none;
         }
+        .grid {
+            display: flex;
+            justify-content: space-between;
+            gap: 3rem;
+            // flex-wrap: wrap;
+        }
+        .col {
+            flex: 0 1 50%;
+        }
         </style>`;
     }
 });
